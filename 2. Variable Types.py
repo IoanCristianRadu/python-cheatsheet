dc6432113e6de47426f7be26a49d9f307cf46562
Cristian <raduioancristian@gmail.com>
2019-03-16 20:54:12 +0100

    Python day one!

---
+++ 2. Variable Types.py
@@ -0,0 +1,15 @@
+name = "Cristian"
+age = 23
+
+print("I am {0}, and I am {1} years old".format(name, age))
+print(f"I am {name}, and I am {age} years old")
+
+a = True
+b = False
+c = None
+
+answer = 42
+pi = 3.14159
+
+print("\n Float to int")
+print(int(pi))
---
+++ 3. If Statements.py
@@ -0,0 +1,27 @@
+number = 5
+if number == 5:
+    print("Number is 5")
+else:
+    print("Number is NOT 5")
+
+if number != 4:
+    print("Number is not 4")
+
+graduate = False
+if not graduate:
+    print("Oh, you are still a student!")
+
+text = "Python"
+if text:
+    print("Text is truthy")
+
+if text and not graduate:
+    print("Text and not graduate!")
+
+none = None
+if text or none:
+    print("Text or none")
+
+a = 1
+b = 2
+print("bigger" if a > b else "smaller")
